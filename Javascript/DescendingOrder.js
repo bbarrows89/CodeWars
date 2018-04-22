93cfab4da48aaaf1ddbb192ad5b22c937a98ee26
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-04-22 13:39:52 -0700

    chained methods together

--- Javascript/DescendingOrder.js
+++ Javascript/DescendingOrder.js
@@ -7,11 +7,7 @@ digits in descending order, returning the largest possible integer from the give
 
 function descendingOrder(n) {
 // convert number to string, split each digit, sort ascending, reverse array, join digits
-  let processed = n.toString();
-  let arr = string.split('');
-  let sorted = arr.sort();
-  let descend = sort.reverse();
-  let joined = descend.join('');
+  let sorted = n.toString().split('').sort().reverse().join('');
 // coerce string to number using +
-  return +joined;
+  return +sorted;
 }
