a5660b59c870951ad7fe04dadc270d8cd771b3f9
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-04-22 13:35:58 -0700

    initial solution

---
+++ Javascript/DescendingOrder.js
@@ -0,0 +1,17 @@
+/* 
+Problem: Descending Order
+
+Description: Write a function that takes a positive integer and returns it's
+digits in descending order, returning the largest possible integer from the given digits 
+*/
+
+function descendingOrder(n) {
+// convert number to string, split each digit, sort ascending, reverse array, join digits
+  let string = n.toString().split('').sort().reverse().join('');
+  let arr = string.split('');
+  let sorted = arr.sort();
+  let descend = sort.reverse();
+  let joined = descend.join('');
+// coerce string to number using +
+  return +joined;
+}
